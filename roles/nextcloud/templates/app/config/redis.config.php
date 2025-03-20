341d81cf9c5c8b0ff9b87a5e928714766875d3f3
Alexander Groß <agross@therightstuff.de>
2025-03-20 20:04:40 +0100

    nextcloud: move to tanker

--- roles/nextcloud/templates/app/config/redis.config.php
+++ roles/nextcloud/templates/app/config/redis.config.php
@@ -1,10 +1,11 @@
 <?php
 $CONFIG = array (
   # https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/config_sample_php_parameters.html#memcache-locking
+  'memcache.local' => '\OC\Memcache\Redis',
+  'memcache.distributed' => '\OC\Memcache\Redis',
   'memcache.locking' => '\OC\Memcache\Redis',
   'redis' => array (
-    'host' => 'redis',
+    'host' => 'valkey',
     'port' => 6379,
-    'password' => '',
   ),
 );
