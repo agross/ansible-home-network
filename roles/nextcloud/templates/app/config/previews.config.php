a9cb81295473ad1706f82c20308800169c03939f
Alexander Groß <agross@therightstuff.de>
2026-06-18 16:09:23 +0200

    nextcloud: review preview support

--- roles/nextcloud/templates/app/config/previews.config.php
+++ roles/nextcloud/templates/app/config/previews.config.php
@@ -1,22 +1,25 @@
 <?php
 $CONFIG = array (
   # https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/config_sample_php_parameters.html#preview-max-x
-  'preview_max_x' => 2048,
-  'preview_max_y' => 2048,
+  'preview_max_x' => 512,
+  'preview_max_y' => 512,
   'preview_max_scale_factor' => 4,
   'preview_imaginary_url' => 'http://imaginary:9000',
+  'preview_format' => 'webp',
   'enabledPreviewProviders' => array (
     'OC\Preview\Imaginary',
-    // Defaults preview providers.
+    'OC\Preview\ImaginaryPDF',
+    // Default preview providers.
     'OC\Preview\PNG',
     'OC\Preview\JPEG',
     'OC\Preview\GIF',
-    'OC\Preview\HEIC',
     'OC\Preview\BMP',
     'OC\Preview\XBitmap',
-    'OC\Preview\MP3',
-    'OC\Preview\TXT',
+    'OC\Preview\Krita',
+    'OC\Preview\WebP',
     'OC\Preview\MarkDown',
+    'OC\Preview\TXT',
+    'OC\Preview\OpenDocument',
     // Extra preview providers.
     'OC\Preview\MSOffice2003',
     'OC\Preview\MSOffice2007',
@@ -24,5 +27,7 @@ $CONFIG = array (
     'OC\Preview\OpenDocument',
     'OC\Preview\PDF',
     'OC\Preview\SVG',
+    'OC\Preview\HEIC',
+    'OC\Preview\MP3',
   )
 );
